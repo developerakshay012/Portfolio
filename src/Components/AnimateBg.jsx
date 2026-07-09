b9c756bd1e13ed711ab77ea5ca0e937d9da41849
Akshay <720akshaykumar@gmail.com>
2026-07-09 23:44:14 +0530

    adding some fitures

--- src/Components/AnimateBg.jsx
+++ src/Components/AnimateBg.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 
 const AnimateBg = () => {
   return (
-    <div className='fixed inset-0 overflow-hidden z-0 pointer-events-none '>
-        <div className='absolute top-1/4 left-1/4 w-64 h-64 md:w-80 rounded-full
-          bg-linear-to-r from-neo-primary to-neo-accent opacity-20 blur-3xl animate-blob'></div>
-        <div className='absolute top-1/3 right-1/4 w-64 h-64 md:w-80 rounded-full
-          bg-linear-to-r from-neo-secondary to-neo-accent opacity-15 blur-3xl  animate-blob animation-delay-4000'></div>
-        <div></div>
-    </div>
+    <div className="fixed inset-0 overflow-hidden z-0 pointer-events-none">
+  {/* Top Left Glow */}
+  <div
+    className="absolute top-1/4 left-1/4 w-72 h-72 md:w-96 md:h-96 rounded-full
+    bg-gradient-to-r from-orange-500 via-amber-400 to-yellow-300
+    opacity-20 blur-3xl animate-blob"
+  ></div>
+
+  {/* Top Right Glow */}
+  <div
+    className="absolute top-1/3 right-1/4 w-72 h-72 md:w-96 md:h-96 rounded-full
+    bg-gradient-to-r from-amber-500 via-orange-400 to-yellow-200
+    opacity-15 blur-3xl animate-blob animation-delay-4000"
+  ></div>
+</div>
   )
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+        {/* navbar */}
+    
+        <motion.nav
+          initial = {{ y: -50 , opacity:0 ,backdropFilter:'blur(0px)' }}
+          animate ={{y:0 , opacity:1 , backdropFilter:'blur(20px)'}}
+          transition={{
+            duration:0.6,
+            ease:"easeInOut"
+          }}
+
+          className='fixed w-full z-50 shadow-xl bg-white/60 dark:bg-zinc-900/60 backdrop-blur-md border border-white/20 dark:border-white/10'
+        >
+
+          <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  py-4'>
+            <div className='flex justify-between items-center'>
+
+              <motion.span
+                initial={{opacity:0 , x:-20}}
+                animate={{opacity:1, x:0}}
+                transition={{duration:0.7}}
+                whileHover={{scale:1.05}}
+                className='text-2xl font-bold cursor-pointer gradient-text'
+              >
+                      AKSHAY
+              </motion.span>
+
+            </div>
+          </div>
+
+        </motion.nav>
+
+
+
+    </>
   )
 }
 
